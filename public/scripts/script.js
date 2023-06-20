69281b0a2c5f674704cf646e0bd06ebdd3d99091
Stefan Espant <info@espant.com>
2023-06-20 12:09:02 +0200

    v1.4
    geluidseffecten werken bij de links

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,12 +1,26 @@
 const interfaceSound = new Audio("./assets/interface-124464.mp3");
-const interfaceClick = document.querySelector(".default-link");
+const interfaceClicks = document.querySelectorAll(".default-link");
 
-interfaceClick.addEventListener("click", (event) => {
+interfaceClicks.forEach((interfaceClick) => {
+  interfaceClick.addEventListener("click", (event) => {
     event.preventDefault(); // Voorkomt dat de standaard linkactie wordt uitgevoerd
 
     interfaceSound.play();
 
     setTimeout(() => {
-        window.location.href = event.target.href; // Doorsturen naar de volgende pagina
+      window.location.href = event.target.href; // Doorsturen naar de volgende pagina
     }, 800); // Wacht 0.8 seconden voordat de doorstuuring plaatsvindt
+  });
 });
+
+
+    const interfaceBackSound = new Audio("../assets/interface-124476.mp3");
+    const interfaceBackClick = document.querySelector(".link-retro");
+    
+    interfaceBackClick.addEventListener("click", (event) => {
+      interfaceBackSound.play();
+  
+      setTimeout(() => {
+        window.location.href = event.target.href; // Doorsturen naar de volgende pagina
+      }, 600); // Wacht 0.6 seconden voordat de doorstuuring plaatsvindt
+    });
