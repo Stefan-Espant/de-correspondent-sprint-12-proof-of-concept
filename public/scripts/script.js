82044632c7f1d2fe880b0b6ec401ea5650df880f
Stefan Espant <info@espant.com>
2023-06-21 10:50:12 +0200

    v1.4.3
    custom properties toegepast

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,26 +1,31 @@
-const interfaceSound = new Audio("./assets/interface-124464.mp3");
-const interfaceClicks = document.querySelectorAll(".default-link");
+const interfaceSound = new Audio(
+	"./assets/interface-124464.mp3"
+);
+const interfaceClicks =
+	document.querySelectorAll(".default-link");
 
 interfaceClicks.forEach((interfaceClick) => {
-  interfaceClick.addEventListener("click", (event) => {
-    event.preventDefault(); // Voorkomt dat de standaard linkactie wordt uitgevoerd
+	interfaceClick.addEventListener("click", (event) => {
+		event.preventDefault(); // Voorkomt dat de standaard linkactie wordt uitgevoerd
 
-    interfaceSound.play();
+		interfaceSound.play();
 
-    setTimeout(() => {
-      window.location.href = event.target.href; // Doorsturen naar de volgende pagina
-    }, 800); // Wacht 0.8 seconden voordat de doorstuuring plaatsvindt
-  });
+		setTimeout(() => {
+			window.location.href = event.target.href; // Doorsturen naar de volgende pagina
+		}, 800); // Wacht 0.8 seconden voordat de doorstuuring plaatsvindt
+	});
 });
 
+const interfaceBackSound = new Audio(
+	"../assets/interface-124476.mp3"
+);
+const interfaceBackClick =
+	document.querySelector(".link-retro");
 
-    const interfaceBackSound = new Audio("../assets/interface-124476.mp3");
-    const interfaceBackClick = document.querySelector(".link-retro");
-    
-    interfaceBackClick.addEventListener("click", (event) => {
-      interfaceBackSound.play();
-  
-      setTimeout(() => {
-        window.location.href = event.target.href; // Doorsturen naar de volgende pagina
-      }, 600); // Wacht 0.6 seconden voordat de doorstuuring plaatsvindt
-    });
+interfaceBackClick.addEventListener("click", (event) => {
+	interfaceBackSound.play();
+
+	setTimeout(() => {
+		window.location.href = event.target.href; // Doorsturen naar de volgende pagina
+	}, 600); // Wacht 0.6 seconden voordat de doorstuuring plaatsvindt
+});
